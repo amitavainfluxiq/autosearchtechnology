c0c40c615bf7fda8011b2dff435882a80d331faf
amitavainfluxiq <amitava.involutiontech@gmail.com>
2016-06-16 18:22:05 +0530

    ast project upload  menu  6/16/2016

--- wp-content/themes/autosearchtechnology/header.php
+++ wp-content/themes/autosearchtechnology/header.php
@@ -67,26 +67,36 @@
                   $pages2 = get_pages($args2);
 
 
-
+                  if ( is_page( $page->ID ) || $post->post_parent == $page->ID ) {
+                      $active = 'active';
+                  } else {
+                      $active = '';
+                  }
 
 
                       if(count($pages2)>0) {
                          // echo ' <li class="ssd dropdown lidiv'.$ic.' " ><a data-toggle="dropdown" class="dropdown-toggle" href="' . get_page_link($page->ID) . '"> ' . $page->post_title . ' </a>';
-                          echo ' <li class="ssd dropdown lidiv'.$ic.' " ><a data-toggle="dropdown" class="dropdown-toggle" href="javascript:void(0)"> ' . $page->post_title . ' </a>';
+                          echo ' <li class="ssd dropdown lidiv'.$ic.' '.$active.' " ><a data-toggle="dropdown" class="dropdown-toggle" href="javascript:void(0)"> ' . $page->post_title . ' </a>';
 
                           echo "<ul class=dropdown-menu>";
 
 
                           foreach($pages2 as $childpage){
 
+                              if ( is_page( $childpage->ID ) ) {
+                                  $active = 'activechild';
+                              } else {
+                                  $active = '';
+                              }
 
-                              echo ' <li class="ln lidiv'.$ic.' "><a href="' . get_page_link($childpage->ID) . '"> ' . $childpage->post_title . ' </a></li>';
+
+                              echo ' <li class="ln lidiv'.$ic.' '.$active.' "><a href="' . get_page_link($childpage->ID) . '"> ' . $childpage->post_title . ' </a></li>';
 
                           }
                           echo "</ul>";
 
                       }else{
-                          echo ' <li class="ssd lidiv'.$ic.' " ><a  class="dropdown-toggle" href="' . get_page_link($page->ID) . '"> ' . $page->post_title . ' </a>';
+                          echo ' <li class="ssd lidiv'.$ic.' '.$active.'" ><a  class="dropdown-toggle" href="' . get_page_link($page->ID) . '"> ' . $page->post_title . ' </a>';
                       }
 
 
